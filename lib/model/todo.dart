ef6c3a3dbd255161fc11895f4d09b33412976996
Aayush Shrestha <aayushshres20@gmail.com>
2023-08-27 16:09:40 +0545

    First Commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,25 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(
+        id: '01',
+        todoText: 'Completed Task',
+        isDone: true,
+      ),
+      ToDo(
+        id: '02',
+        todoText: 'Incomplete Task',
+      ),
+    ];
+  }
+}
